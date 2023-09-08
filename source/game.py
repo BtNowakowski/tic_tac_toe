7dbb1bb262add7867b60a9171fad008f5c8b4a9e
YOUR NAME <YOUR EMAIL>
2023-09-08 09:28:38 +0200

    Added computer as a player To make it not as boring as it was

--- source/game.py
+++ source/game.py
@@ -1,5 +1,6 @@
 import time
 from utils import draw_board, clear_screen, whose_turn, check_winner
+from random import randint as random_int
 
 
 class Game:
@@ -10,6 +11,36 @@ class Game:
             ["_", "_", "_"],
         ]
         self.player = ""
+
+    def position_available(self, choice):
+        if int(choice) <= 3:
+            field_chosen = self.board[0][int(choice) - 1]
+            if field_chosen == "_":
+                self.board[0][int(choice) - 1] = self.player
+                return True
+            print("Invalid choice. Try again!")
+        elif int(choice) <= 6:
+            field_chosen = self.board[1][int(choice) - 4]
+            if field_chosen == "_":
+                self.board[1][int(choice) - 4] = self.player
+                return True
+            print("Invalid choice. Try again!")
+        elif int(choice) <= 9:
+            field_chosen = self.board[2][int(choice) - 7]
+            if field_chosen == "_":
+                self.board[2][int(choice) - 7] = self.player
+                return True
+            print("Invalid choice. Try again!")
+        return False
+
+    def computer_move(self):
+        if "_" in self.board[0] or "_" in self.board[1] or "_" in self.board[2]:
+            position = random_int(1, 9)
+
+            if self.position_available(position):
+                print(f"Computer taken place--> {position}")
+            else:
+                self.computer_move()
 
     def ask_verify_choice(self):
         while True:
@@ -22,24 +53,8 @@ class Game:
                 print("\nBye!")
                 exit(0)
 
-            if int(choice) <= 3:
-                field_chosen = self.board[0][int(choice) - 1]
-                if field_chosen == "_":
-                    self.board[0][int(choice) - 1] = self.player
-                    break
-                print("Invalid choice. Try again!")
-            elif int(choice) <= 6:
-                field_chosen = self.board[1][int(choice) - 4]
-                if field_chosen == "_":
-                    self.board[1][int(choice) - 4] = self.player
-                    break
-                print("Invalid choice. Try again!")
-            elif int(choice) <= 9:
-                field_chosen = self.board[2][int(choice) - 7]
-                if field_chosen == "_":
-                    self.board[2][int(choice) - 7] = self.player
-                    break
-                print("Invalid choice. Try again!")
+            if self.position_available(choice):
+                break
 
     def main_loop(self):
         for i in range(9):
@@ -49,7 +64,10 @@ class Game:
 
             self.player = whose_turn(i)
 
-            self.ask_verify_choice()
+            if self.player == "O":
+                self.computer_move()
+            else:
+                self.ask_verify_choice()
 
             did_win, self.board = check_winner(self.board)
 
